89ee7385c592e40b703207f2617f2a4392acbdef
samnang-an <samnang.an7@gmail.com>
2024-02-04 15:24:14 -0600

    feat: add more CRUD api

--- src/main/java/com/ankaboot/AuthenticationServer/dataaccess/repository/UserRepository.java
+++ src/main/java/com/ankaboot/AuthenticationServer/dataaccess/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ankaboot.AuthenticationServer.dataaccess.repository;
 
 import com.ankaboot.AuthenticationServer.dataaccess.document.User;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User,Long> {
 
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
 
   void deleteByUsername(String username);
 }
--- src/main/java/com/ankaboot/AuthenticationServer/dataaccess/service/UserService.java
+++ src/main/java/com/ankaboot/AuthenticationServer/dataaccess/service/UserService.java
@@ -2,6 +2,7 @@ package com.ankaboot.AuthenticationServer.dataaccess.service;
 
 import com.ankaboot.AuthenticationServer.dto.UserDto;
 import java.util.List;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
 
@@ -9,9 +10,13 @@ public interface UserService {
 
   UserDto updateUser(String username, String passowrd, List<String> roles);
 
+  UserDto updateUser(long id, String username, String passowrd, List<String> roles);
+
   void deleteUser(String username);
 
   UserDto fetchUser(String username);
 
   List<UserDto> findAll();
+
+  void deleteUser(long userId);
 }
