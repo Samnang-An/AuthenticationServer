72f25ec22e5de4909f211958698343eefa9bfd01
samnang-an <samnang.an7@gmail.com>
2024-02-04 22:23:29 -0600

    refactor: reformat code

--- src/main/java/com/ankaboot/AuthenticationServer/AuthenticationServerApplication.java
+++ src/main/java/com/ankaboot/AuthenticationServer/AuthenticationServerApplication.java
@@ -1,21 +1,12 @@
 package com.ankaboot.AuthenticationServer;
 
 import com.ankaboot.AuthenticationServer.dataaccess.service.UserService;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableResourceServer
