f338d0ccf4631397dd129e680c102d880a0e50db
samnang-an <samnang.an7@gmail.com>
2024-02-07 12:30:10 -0600

    refactor: add dependency

--- src/main/java/com/ankaboot/AuthenticationServer/AuthenticationServerApplication.java
+++ src/main/java/com/ankaboot/AuthenticationServer/AuthenticationServerApplication.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableDiscoveryClient
 public class AuthenticationServerApplication implements CommandLineRunner {
 
   @Autowired
